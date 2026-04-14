576ef30689fbc584a9629f8a90947f4785dd47f3
GamePad <hejdjdjuduf@gmail.com>
2026-04-14 13:34:46 +0300

    Add multiply command with test

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ def add(a, b):
     return a + b
 def subtract(a, b):   # <-- Новая функция
     return a - b
+def multiply(a, b):
+    return a * b
 if __name__ == "__main__":
     print(f"2 + 2 = {add(2, 2)}")
     print(f"10 - 3 = {subtract(10, 3)}") # <-- Новая строка
+    print(f"5 * 4 = {multiply(5, 4)}")
